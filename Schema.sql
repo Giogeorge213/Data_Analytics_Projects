6c871c87af2088de8add59395c023e80b91a0056
Giogeorge213 <98611938+Giogeorge213@users.noreply.github.com>
2024-04-29 21:25:49 -0500

    Create Schema.sql

---
+++ Schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE your_table_name (
+    store_location GEOGRAPHY,
+    county_number STRING,
+    county STRING,
+    category STRING,
+    category_name STRING,
+    vendor_number STRING,
+    vendor_name STRING,
+    item_number STRING,
+    item_description STRING,
+    pack INTEGER,
+    bottle_volume_ml INTEGER,
+    state_bottle_cost FLOAT,
+    state_bottle_retail FLOAT,
+    bottles_sold INTEGER,
+    sale_dollars FLOAT,
+    volume_sold_liters FLOAT,
+    volume_sold_gallons FLOAT
+);
